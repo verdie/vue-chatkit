60b5aa7e7317cf62414d0af7f98673dc255993ea
halynaburdiian <verdamtion@gmail.com>
2019-11-06 14:10:07 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+/* eslint-disable eol-last */
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueChatScroll from 'vue-chat-scroll'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VueChatScroll)
 
 new Vue({
   router,
